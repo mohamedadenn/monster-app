65717d324aebe923cf09d01dc995b5fb4d068b41
mohamed <mohamed.aden@live.ca>
2020-09-12 00:06:08 -0400

    Added Search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,32 @@ class App extends Component {
    constructor() {
        super();
        this.state = {
-           monsters: []
+           monsters: [],
+           searchField: ''
        };
    }
 
    componentDidMount() {
-       console.log('hi')
        fetch('https://jsonplaceholder.typicode.com/users')
        .then(response => response.json())
        .then(users => this.setState({monsters: users}));
    }
    
     render() {
+        const { monsters, searchField } = this.state;
+        const filteredMonsters = monsters.filter(monster =>
+            monster.name.toLowerCase().includes(searchField.toLowerCase())
+            );
+
         return (
             <div className="App">
-            <CardList monsters={this.state.monsters} />
-
-    </div>
+            <input 
+            type='search' 
+            placeholder='search monsters'
+            onChange={e => this.setState({searchField: e.target.value})}
+             />
+            <CardList monsters={filteredMonsters} />
+            </div>
         );
     }
 }
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import './card-list.styles.css';
 
 //Components
-import {Card} from '../card/card.component';
+import { Card } from '../card/card.component';
 
-export const CardList = (props) => (
-         <div className="card-list">
+export const CardList = (props) => ( <
+    div className = "card-list" > {
+        props.monsters.map(monster => ( <
+            Card key = { monster.id }
+            monster = { monster }
+            />
+        ))
+    }
 
-         {props.monsters.map(monster => (
-             <Card key={monster.id} monster={monster} />
-        ))}
-        
-        </div>
-   
+    <
+    /div>
+
 );
