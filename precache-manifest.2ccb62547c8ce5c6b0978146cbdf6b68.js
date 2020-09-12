aa2b24ad083a342cf5c3a81dbd5a7a940f1f8c97
mohamed <mohamed.aden@live.ca>
2020-09-12 16:38:29 -0400

    Updates

---
+++ precache-manifest.2ccb62547c8ce5c6b0978146cbdf6b68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e59f2a2ffc9f2aee7ba568d681f30020",
+    "url": "/monsters-app/index.html"
+  },
+  {
+    "revision": "c53fd9a257fa94685125",
+    "url": "/monsters-app/static/css/main.3465edd2.chunk.css"
+  },
+  {
+    "revision": "17e7a3acfa6de3ea2ab4",
+    "url": "/monsters-app/static/js/2.faf01414.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-app/static/js/2.faf01414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c53fd9a257fa94685125",
+    "url": "/monsters-app/static/js/main.d3b6865b.chunk.js"
+  },
+  {
+    "revision": "7138d42281881b672924",
+    "url": "/monsters-app/static/js/runtime-main.ce03e1fd.js"
+  }
+]);
